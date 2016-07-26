25d775df4608e6126d5228a8519408d44ea01a0d
liuwanglei <liuwanglei@zhen-de.com>
2016-07-26 17:55:58 +0800

    add new project

---
+++ L02/L02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  L02
+//
+//  Created by 刘旺磊 on 16/7/26.
+//  Copyright © 2016年 刘旺磊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
